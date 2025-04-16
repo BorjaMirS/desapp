1ef667ac71d72cca9877da19285c215e948dc1a8
Borja Miranda <bmirandas@iescapdepera.com>
2025-04-16 11:35:30 +0200

    Added logic to show or hide despeses

--- src/App.jsx
+++ src/App.jsx
@@ -12,39 +12,45 @@ function App() {
 
   // Creació dels primers components dinàmics
   //let titol = "Benvinguts al curs"
-  const [titol, setTitol] = useState("Benvinguts al curs")
-  const subtitol = "React & Firebase"
-  const web = "https://www.menorca.info"
+  const [mostrarDespeses, setMostrarDespeses] = useState(true)
   const [despeses, setDespeses] = useState([
     {concepte: "dinar", quantia: 50.67, pagatPer:"Pere", id:1},
     {concepte: "sopar", quantia: 33.33, pagatPer:"Toni", id:2},
     {concepte: "excursió", quantia: 150.27, pagatPer:"Anna", id:3}
   ])
 
-  const handleClick = (e) => {
-    setTitol("Nou títol")
-    console.log(titol)
+  console.log(mostrarDespeses)
+
+  const handleClick = (id) => {
+    // console.log(id)
+    //setDespeses(despeses.filter((despesa) => id !== despesa.id))
+    setDespeses((despesesPrevies) => {
+      return despesesPrevies.filter((despesa) => id !== despesa.id)
+    })
   }
 
   return (
     <div>
-      <h2> { titol }</h2>
-      <p> { subtitol }</p>
-      <div>
-        <img src={ logoReact } alt="react" />
-      </div>
-      <div>
-        <img src="./vite.svg" alt="vite" />
-      </div>
-      <p>La data d'avui: { new Date().toDateString() }</p>
-      <p>Número aleatori: { Math.random() * 100 }</p>
-      <a href={ web }>Visita es diari</a>
-      <button onClick={handleClick}>Fes clic</button>
+     {  !mostrarDespeses && 
+       (
+          <div>
+            <button onClick={ () => setMostrarDespeses(true)}>Mostrar Despeses</button>
+          </div>
+        )
+     } 
+     {  mostrarDespeses && 
+       (
+          <div>
+            <button onClick={ () => setMostrarDespeses(false)}>Ocultar Despeses</button>
+          </div>
+        )
+     } 
       { 
       //Index és un atribut per defecte de map
-        despeses.map((despesa, index) => (
+        mostrarDespeses && despeses.map((despesa, index) => (
             <div key={despesa.id}>
               <h2>{index + 1} - {despesa.concepte} </h2>
+              <button onClick={ () => handleClick(despesa.id)}>Eliminar despesa</button>
             </div> 
           )
         )
