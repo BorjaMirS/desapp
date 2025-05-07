b091d810b22d29c5e449eeeb1e1d28f057fd0ca2
Borja Miranda <bmirandas@iescapdepera.com>
2025-05-07 09:41:46 +0200

    Handle variable en línea setConcepte amb el param e funcio anònima

--- src/components/despesaForm/DespesaForm.jsx
+++ src/components/despesaForm/DespesaForm.jsx
@@ -1,17 +1,20 @@
+import { useState } from 'react';
 import './DespesaForm.css'
 
 
 export default function DespesaForm() {
 
+  const [concepte, setConcepte] = useState("");
+
   const handleChange = (e) => {
     console.log(e.target.value)
   }; 
-  
+
   return (
     <form className="despesa-form">
         <label>
             <span>Concepte</span>  
-            <input type="text" onChange={handleChange}/>          
+            <input type="text" onChange={(e) => setConcepte(e.target.value)}/>          
         </label>
         <label>
             <span>Quantia</span>  
