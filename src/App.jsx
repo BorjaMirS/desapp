297bc61935cbecfd88b59f0e015f78ca30eb1929
Borja Miranda <bmirandas@iescapdepera.com>
2025-05-21 10:45:35 +0200

    Moved navigation to Inici.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 
 import React, { useState, useEffect } from 'react'
 import './App.css'
-import Titol from './components/titol/Titol'
-import Modal from './components/modal/Modal'
-import DespesesLlista from './components/despesesllista/despesesllista'
-import DespesaForm from './components/despesaForm/DespesaForm'
-import { saveDespesa, onGetDespeses, deleteDespesa } from './firebase/firebase'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Inici from './pages/inici/inici'
 import Login from './pages/login/login'
@@ -21,90 +16,16 @@ function App() {
 
   // Creació dels primers components dinàmics
   //let titol = "Benvinguts al curs"
-  const [mostrarDespeses, setMostrarDespeses] = useState(true)
-  const [mostraModal, setMostraModal] = useState(false)
-  const [despeses, setDespeses] = useState(null)
 
-  const [filtrarPerQuantia, setFiltrarPerQuantia] = useState(false)
     
 
   //const [despeses, setDespeses] = useState([])
 
   //console.log(mostrarDespeses)
-  console.log(mostraModal)
 
   //Use effect
 
-  const subtitol = "React & Firebase!!"
 
- useEffect(() => {
-     onGetDespeses((querySnapshot) => {
-      let resultats = []
-
-      querySnapshot.forEach((doc) => {
-        const despesa = doc.data()
-        despesa.id = doc.id
-        resultats.push({...doc.data(), id: doc.id})
-      })
-
-      setDespeses(resultats)
-    })
- },[])
-
-  useEffect(() => {
-    setDespeses((despesesPrevies) => {
-      if (filtrarPerQuantia)
-        return despesesPrevies.filter((despesa) => despesa.quantia > 10.00)
-      else 
-        return despesesPrevies
-    })
-  }
-   , [filtrarPerQuantia]
-  )
-
-  const afegirDespesa = (despesa) => {
-      setDespeses((despesesPrevies) => {
-
-        saveDespesa(despesa)
-         .then((idDespesa) => {
-            despesa.id = idDespesa
-
-            if (!despesesPrevies) {
-              return [despesa]
-            } else {
-              return [...despesesPrevies, despesa]
-            }
-         })
-
-      })
-
-      setMostraModal(false)
-  }
-
-
-  const eliminarDespesa = (id) => {
-    setDespeses((despesesPrevies) => {
-      deleteDespesa(id).then(() => {
-
-      })
-    })
-
-    setMostraModal(false)
-}
-
-  const handleClick = (id) => {
-    // console.log(id)
-    //setDespeses(despeses.filter((despesa) => id !== despesa.id))
-    setDespeses((despesesPrevies) => {
-      return despesesPrevies.filter((despesa) => id !== despesa.id)
-    })
-  }
-
-  const handleTancar = () => {
-    setMostraModal(false)
-  }
-
-  
   return (
     <div>
       <Routes>
@@ -112,34 +33,6 @@ function App() {
         <Route path='/login' element={ <Login />} />
       </Routes>
     <Navbar />
-      <Titol titol="Benvinguts al curs!!" subtitol={subtitol} />
-     {  !mostrarDespeses && 
-       (
-          <div>
-            <button onClick={ () => setMostrarDespeses(true)}>Mostrar Despeses</button>
-          </div>
-        )
-     } 
-     {  mostrarDespeses && 
-       (
-          <div>
-            <button onClick={ () => setMostrarDespeses(false)}>Ocultar Despeses</button>
-          </div>
-        )
-     } 
-      { 
-      //Index és un atribut per defecte de map
-        despeses && <DespesesLlista despeses={despeses} handleClick={handleClick}/>
-      }
-      { mostraModal && <Modal handleTancar = {handleTancar}>  
-          <DespesaForm afegirDespesa={afegirDespesa}/>
-      </Modal>  }
-      <div>
-        <button onClick={ () => setMostraModal(true)}> Afegir despesa </button>
-      </div>
-      <div>
-        <button onClick={() => setFiltrarPerQuantia(true)}>Filtrar</button>
-      </div>
     </div>
   )
 }
--- src/pages/inici/Inici.jsx
+++ src/pages/inici/Inici.jsx
@@ -1,6 +1,119 @@
 
+import Titol from '../../components/titol/Titol'
+import Modal from '../../components/modal/Modal'
+import DespesesLlista from '../../components/despesesllista/despesesllista'
+import DespesaForm from '../../components/despesaForm/DespesaForm'
+import { saveDespesa, onGetDespeses, deleteDespesa } from '../../firebase/firebase'
+import { useState, useEffect } from 'react'
+
 export default function inici() {
+
+      const [mostrarDespeses, setMostrarDespeses] = useState(true)
+      const [mostraModal, setMostraModal] = useState(false)
+      const [despeses, setDespeses] = useState(null)
+    
+      const [filtrarPerQuantia, setFiltrarPerQuantia] = useState(false)
+
+    const subtitol = "React & Firebase!!"
+
+      useEffect(() => {
+           onGetDespeses((querySnapshot) => {
+            let resultats = []
+      
+            querySnapshot.forEach((doc) => {
+              const despesa = doc.data()
+              despesa.id = doc.id
+              resultats.push({...doc.data(), id: doc.id})
+            })
+      
+            setDespeses(resultats)
+          })
+       },[])
+      
+        useEffect(() => {
+          setDespeses((despesesPrevies) => {
+            if (filtrarPerQuantia)
+              return despesesPrevies.filter((despesa) => despesa.quantia > 10.00)
+            else 
+              return despesesPrevies
+          })
+        }
+         , [filtrarPerQuantia]
+        )
+      
+        const afegirDespesa = (despesa) => {
+            setDespeses((despesesPrevies) => {
+      
+              saveDespesa(despesa)
+               .then((idDespesa) => {
+                  despesa.id = idDespesa
+      
+                  if (!despesesPrevies) {
+                    return [despesa]
+                  } else {
+                    return [...despesesPrevies, despesa]
+                  }
+               })
+      
+            })
+      
+            setMostraModal(false)
+        }
+      
+      
+        const eliminarDespesa = (id) => {
+          setDespeses((despesesPrevies) => {
+            deleteDespesa(id).then(() => {
+      
+            })
+          })
+      
+          setMostraModal(false)
+      }
+
+      const handleClick = (id) => {
+        // console.log(id)
+        //setDespeses(despeses.filter((despesa) => id !== despesa.id))
+        setDespeses((despesesPrevies) => {
+          return despesesPrevies.filter((despesa) => id !== despesa.id)
+        })
+      }
+    
+      const handleTancar = () => {
+        setMostraModal(false)
+      }
+
   return (
-    <div><h1>Inici</h1></div>
+    <div>
+            <h1>Inici</h1>
+             {  !mostrarDespeses && 
+               (
+                  <div>
+                    <button onClick={ () => setMostrarDespeses(true)}>Mostrar Despeses</button>
+                  </div>
+                )
+             } 
+             {  mostrarDespeses && 
+               (
+                  <div>
+                    <button onClick={ () => setMostrarDespeses(false)}>Ocultar Despeses</button>
+                  </div>
+                )
+             } 
+                   <Titol titol="Benvinguts al curs!!" subtitol={subtitol} />
+            { 
+            //Index és un atribut per defecte de map
+                despeses && <DespesesLlista despeses={despeses} eliminarDespesa={eliminarDespesa}/>
+            }
+            { mostraModal && <Modal handleTancar = {handleTancar}>  
+                <DespesaForm afegirDespesa={afegirDespesa}/>
+            </Modal>  }
+            <div>
+                <button onClick={ () => setMostraModal(true)}> Afegir despesa </button>
+            </div>
+            <div>
+                <button onClick={() => setFiltrarPerQuantia(true)}>Filtrar</button>
+            </div>
+    </div>
   )
 }
