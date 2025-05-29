06d20b6f03e646d9d1312249bfecb6cedc24f246
Borja Miranda <bmirandas@iescapdepera.com>
2025-05-29 08:15:39 +0200

    Beginning of AfegirProjecte

---
+++ src/components/projectForm/ProjectForm.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import './ProjectForm.css'
+import { saveCollection } from '../../firebase/firebase';
+
+
+export default function ProjectForm({projecte}) {
+
+  const [name, setName] = useState("")
+  const [quantia, setQuantia] = useState("")
+  const [pagatPer, setPagatPer] = useState("")
+  const [participants, setParticipants] = useState(null)
+
+  const resetForm = () => {
+    setName("")
+    setQuantia("")
+    setPagatPer("")
+  }
+
+  const handleChange = (e) => {
+    console.log(e.target.value)
+  }; 
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const projecte = {
+        nom: name,
+        participants: ["pepe", "tomeu"]
+    }
+
+    saveCollection("projectes", projecte);
+
+    resetForm()
+  }
+/*
+         const afegirDespesa = (despesa) => {
+              setDespeses((despesesPrevies) => {
+        
+                saveDespesa(despesa)
+                 .then((idDespesa) => {
+                    despesa.id = idDespesa
+        
+                    if (!despesesPrevies) {
+                      return [despesa]
+                    } else {
+                      return [...despesesPrevies, despesa]
+                    }
+                 })
+        
+              })
+        
+              setMostraModal(false)
+          }
+*/
+  return (
+    <div>
+        <form className="projecte-form" onSubmit={handleSubmit}>
+            <label>
+                <span>Nom del projecte</span>  
+                <input type="text" onChange={(e) => setName(e.target.value)} value={name}/>          
+            </label>
+            <label>
+                <span>Participants</span>  
+                <select onChange={(e) => {setPagatPer(e.target.value)}}>
+                <option value="joan">Joan</option>
+                <option value="borja">Borja</option>
+                <option value="david">David</option>
+                <option value="pere">Pere</option>
+                </select>
+            </label>
+            <button>Crear</button>
+            <p onClick={resetForm}>Restablir els valors inicials</p>
+        </form>
+    </div>
+
+  )
+}
--- src/pages/projectes/Projectes.jsx
+++ src/pages/projectes/Projectes.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import {useState} from 'react'
+import Modal from '../../components/modal/Modal';
+import ProjectForm from '../../components/projectForm/ProjectForm';
+import { saveCollection } from '../../firebase/firebase';
 
 export default function Projectes() {
+
+  const [mostraModal, setMostraModal] = useState(false);
+  const [projectes, setProjectes] = useState(null);
+
+  const handleTancar = () => {
+    setMostraModal(false);
+  }
+
+  const afegirProjecte = (projecte) => {
+    setProjectes((projectesPrevis) => {
+    saveCollection("projectes", projecte)
+      .then((idProjecte) => {
+        projecte.id = idProjecte
+        
+        if (!projectesPrevis) {
+            return [projecte]
+        } else {
+            return [...projectesPrevis, projecte]
+          }
+        })
+        
+    })
+        
+    setMostraModal(false)
+  }
+
   return (
-    <div>Projectes</div>
+    <div>
+        <h1>Projectes</h1>
+        <button onClick={() => setMostraModal(true)}>Crear nou projecte</button>
+        {
+          mostraModal && <Modal handleTancar={handleTancar}>
+                            <ProjectForm afegirProjectes={afegirProjectes}/>
+                        </Modal>
+        }
+    </div>
   )
 }
