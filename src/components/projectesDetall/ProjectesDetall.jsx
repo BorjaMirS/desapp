91a6c72fd9dd0946dd18084226ad30c5e0a3583c
Borja Miranda <bmirandas@iescapdepera.com>
2025-05-31 11:02:58 +0200

    participants del projecte

--- src/components/projectesDetall/ProjectesDetall.jsx
+++ src/components/projectesDetall/ProjectesDetall.jsx
@@ -1,38 +1,58 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { OnGetDocument } from '../../firebase/firebase';
+import Modal from '../modal/Modal';
+import ParticipantsForm from '../participantsForm/ParticipantsForm';
 
 export default function ProjectesDetall() {
     const { id } = useParams();
     const [projecte, setProjecte] = useState(null);
     const [userName, setUserName] = useState("");
+    const [mostraModal, setMostraModal] = useState(false);
+
     
     useEffect(() => {
         const unsubscribe = OnGetDocument(id, "projectes", (proj) => {
-            console.log("Projecte abans: ",proj.data());
             if (proj.exists()) {
-                setProjecte(proj.data());
-                console.log("Projecte després: ", proj.data());
-                console.log("Aquest es un projecte", projecte);
+                setProjecte({...proj.data(), id:proj.id});
+                console.log(`Projecte recuperat amd id: ${proj.id} i informació: ${proj.data()}`);
             } else {
                 setProjecte(null);
             }
         })
         return () => unsubscribe()
-    }, [])
+    }, [id])
 
     if (!projecte) {
         return <p>Projecte no trobat</p>    
     }
     
+  const handleTancar = () => {
+    setMostraModal(false);
+  }
+
   return (
     <div>
         <h2>
             Detall del projecte
         </h2>
         <p><strong> Nom: </strong>{projecte.nom}</p>
+        <p><strong> ID projecte: </strong>{projecte.id}</p>
         <p><strong> ID Propietari: </strong>{projecte.idpropietari}</p>
-        <p><strong> Nom Propietari: </strong>{projecte.nompropietari}</p>
+        <p><strong> Participants: </strong></p>
+        <ul>
+            {projecte.participants && projecte.participants.length > 0 ? 
+                (projecte.participants.map((participant, index) => (
+                 <li key={index}>{participant}</li>))) : 
+                 (<li>No hi ha participants</li>)
+            }
+        </ul>
+        <button onClick={() => setMostraModal(true)}>Gestionar participants</button>
+        {
+          mostraModal && <Modal handleTancar={handleTancar}>
+                            <ParticipantsForm/>
+                        </Modal>
+        }        
     </div>
     
   )
